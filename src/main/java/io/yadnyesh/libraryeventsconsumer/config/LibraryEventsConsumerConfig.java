3f56823774fa814cd08f57a2700a1ab0163940aa
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-05-12 19:59:29 +0530

    Spring Boot Kafka Consumer Record - Manual Offset Management

--- src/main/java/io/yadnyesh/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ src/main/java/io/yadnyesh/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,10 +1,36 @@
 package io.yadnyesh.libraryeventsconsumer.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
-
+	
+	
+	@Bean
+	ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+			ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+			ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+		ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		configurer.configure(factory, kafkaConsumerFactory);
+		//factory.setConcurrency(3);
+		//factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+//		factory.setErrorHandler(((thrownException, data) -> {
+//			log.info("Exception in consumerConfig is {} and the record is {}", thrownException.getMessage(), data);
+//			//persist
+//		}));
+		return factory;
+	}
+	
 }
